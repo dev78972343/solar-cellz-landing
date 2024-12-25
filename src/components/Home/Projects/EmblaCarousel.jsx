ce4d5369ffd08e179c08dd4e4a1019a351f82a71
dev78972343 <developer78972343@gmail.com>
2024-12-25 16:46:46 +0600

    fix: Responsive issue on projects section

--- src/components/Home/Projects/EmblaCarousel.jsx
+++ src/components/Home/Projects/EmblaCarousel.jsx
@@ -41,19 +41,23 @@ const EmblaCarousel = (props) => {
 
               <div className="embla__slide__inner">
                 <img
-                  className="absolute left-14 top-10 hidden sm:block"
+                  className="absolute left-4 top-4 w-20 sm:left-14 sm:top-10 sm:block sm:w-24 lg:w-32"
                   src={slide.logo}
                   alt="Your alt text"
                 />
 
                 <div className="absolute bottom-2 left-6 right-2 flex items-center gap-6 rounded-md border border-gray-300 p-2 text-white sm:bottom-10 sm:left-14 sm:right-10 sm:p-4 lg:p-6">
-                  <p className="order-0 hidden flex-grow flex-col gap-4 md:flex">
-                    <span className="text-6xl font-semibold">{slide.stat}</span>
-                    <span>{slide.description}</span>
+                  <p className="order-0 hidden flex-col gap-4 sm:flex">
+                    <span className="md:text-4x text-lg font-semibold sm:text-2xl lg:text-6xl">
+                      {slide.stat}
+                    </span>
+                    <span className="sm:text-md text-sm">
+                      {slide.description}
+                    </span>
                   </p>
 
-                  <div className="order-1 flex flex-grow-0 flex-col justify-between gap-4 md:gap-8 lg:gap-12">
-                    <p className="sm:text-md text-sm font-medium xl:text-lg">{`"${slide.quote}"`}</p>
+                  <div className="order-1 flex flex-grow-0 flex-col justify-between gap-2 sm:gap-4 md:gap-8 lg:gap-12">
+                    <p className="sm:text-md line-clamp-2 text-sm font-medium sm:line-clamp-none xl:text-lg">{`"${slide.quote}"`}</p>
 
                     <div className="sm:text-md flex items-center justify-between text-sm">
                       <p className="">{slide.author}</p>
