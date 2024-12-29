bfbdbf5bceb9f3e179e785cefda26b68221f2e15
dev78972343 <developer78972343@gmail.com>
2024-12-29 23:37:13 +0600

    Upgrade project section

--- src/components/Home/Projects/EmblaCarousel.jsx
+++ src/components/Home/Projects/EmblaCarousel.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef } from "react";
 import {
   PrevButton,
   NextButton,
@@ -5,9 +6,12 @@ import {
 } from "./EmblaCarouselArrowButtons";
 import useEmblaCarousel from "embla-carousel-react";
 
+const TWEEN_FACTOR_BASE = 0.84;
+
 const EmblaCarousel = (props) => {
   const { slides, options } = props;
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const tweenFactor = useRef(0);
 
   const {
     prevBtnDisabled,
@@ -16,11 +20,67 @@ const EmblaCarousel = (props) => {
     onNextButtonClick,
   } = usePrevNextButtons(emblaApi);
 
+  const numberWithinRange = (number, min, max) =>
+    Math.min(Math.max(number, min), max);
+
+  const setTweenFactor = useCallback((emblaApi) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  }, []);
+
+  const tweenOpacity = useCallback((emblaApi, eventName) => {
+    const engine = emblaApi.internalEngine();
+    const scrollProgress = emblaApi.scrollProgress();
+    const slidesInView = emblaApi.slidesInView();
+    const isScrollEvent = eventName === "scroll";
+
+    emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+      let diffToTarget = scrollSnap - scrollProgress;
+      const slidesInSnap = engine.slideRegistry[snapIndex];
+
+      slidesInSnap.forEach((slideIndex) => {
+        if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+        if (engine.options.loop) {
+          engine.slideLooper.loopPoints.forEach((loopItem) => {
+            const target = loopItem.target();
+
+            if (slideIndex === loopItem.index && target !== 0) {
+              const sign = Math.sign(target);
+
+              if (sign === -1) {
+                diffToTarget = scrollSnap - (1 + scrollProgress);
+              }
+              if (sign === 1) {
+                diffToTarget = scrollSnap + (1 - scrollProgress);
+              }
+            }
+          });
+        }
+
+        const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current);
+        const opacity = numberWithinRange(tweenValue, 0.4, 1).toString();
+        emblaApi.slideNodes()[slideIndex].style.opacity = opacity;
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    setTweenFactor(emblaApi);
+    tweenOpacity(emblaApi);
+    emblaApi
+      .on("reInit", setTweenFactor)
+      .on("reInit", tweenOpacity)
+      .on("scroll", tweenOpacity)
+      .on("slideFocus", tweenOpacity);
+  }, [emblaApi, tweenOpacity]);
+
   return (
     <section className="embla rounded-xl">
       <div className="embla__controls">
         <h2 className="text-left text-3xl font-bold text-gray-800 md:text-4xl">
-        The best companies <br /> say about us
+          The best companies <br /> say about us
         </h2>
 
         <div className="embla__buttons">
