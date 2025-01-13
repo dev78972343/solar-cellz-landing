69fbd76a80c1ea1dc2e2f55303f2fcc64c4e5a74
dev78972343 <developer78972343@gmail.com>
2025-01-14 02:06:12 +0600

    added multilanguage for brands section

--- src/components/Home/Brands/Brands.jsx
+++ src/components/Home/Brands/Brands.jsx
@@ -1,13 +1,15 @@
 import ResponsiveContainer from "@/components/ResponsiveContainer/ResponsiveContainer";
 import BrandsLogoCarousel from "./BrandsLogoCarousel";
 import { cn } from "@/lib/utils";
+import { useTranslation } from "react-i18next";
 
 export default function Brands({ className }) {
+  const { t } = useTranslation();
   return (
     <div className={cn(className, "bg-gray-100")}>
       <ResponsiveContainer>
         <div className="py-10 flex md:flex-row flex-col gap-8 items-center">
-          <h3 className="text-2xl font-bold text-gray-800">Liquidity Supported by</h3>
+          <h3 className="text-2xl font-bold text-gray-800">{t("brands.title")}</h3>
          <BrandsLogoCarousel />
         </div>
       </ResponsiveContainer>
