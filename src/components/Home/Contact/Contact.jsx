86523c5c325b524607d7f8bb04e7c2f124581a89
dev78972343 <developer78972343@gmail.com>
2025-01-15 22:58:34 +0600

    feat:  multilang into contact section

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -4,71 +4,73 @@ import { Input } from "@/components/ui/input";
 import { MailsIcon, Home, PhoneCall, MapPin } from "lucide-react";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
+import { useTranslation } from "react-i18next";
 
 export default function Contact({ className }) {
+  const { t } = useTranslation();
   return (
     <ResponsiveContainer
       classes={cn(className, "flex flex-col items-start justify-center gap-10 md:flex-row")}
     >
       {/* Form Section */}
       <div className="w-full rounded-lg bg-[#dff2fe] p-4 lg:p-8 shadow md:w-1/2">
-        <h2 className="mb-4 text-2xl font-bold">Fill the form</h2>
+        <h2 className="mb-4 text-2xl font-bold">{t("contact.form.title")}</h2>
 
         <form className="grid grid-cols-2 gap-4">
           {/* First Name */}
-          <InputField label="First Name" id="firstName" type="text" placeholder="First Name" />
+          <InputField label={t(`contact.form.fields.0.label`)} id="firstName" type="text" placeholder={t(`contact.form.fields.0.placeholder`)} />
 
           {/* Last Name */}
-          <InputField label="Last Name" id="lastName" type="text" placeholder="Last Name" />
+          <InputField label={t(`contact.form.fields.1.label`)} id="lastName" type="text" placeholder={t(`contact.form.fields.1.placeholder`)} />
 
           {/* Phone Number */}
-          <InputField label="Phone Number" id="phone" type="text" placeholder="Your Phone Number" />
+          <InputField label={t(`contact.form.fields.2.label`)} id="phone" type="text" placeholder={t(`contact.form.fields.2.placeholder`)} />
 
           {/* Email */}
-          <InputField label="Email Address" id="email" type="email" placeholder="Your email address" />
+          <InputField label={t(`contact.form.fields.3.label`)} id="email" type="email" placeholder={t(`contact.form.fields.3.placeholder`)} />
 
           {/* Message */}
           <div className="col-span-2">
             <label htmlFor="message" className="mb-1 block text-sm text-gray-700">
-              Message
+              {t(`contact.form.fields.4.label`)}
             </label>
             <Textarea
               id="message"
               rows="4"
-              placeholder="Your message"
+              placeholder={t(`contact.form.fields.4.placeholder`)}
               className="w-full resize-none border-[#545454] bg-white text-black"
             />
           </div>
 
           {/* Submit Button */}
           <div className="col-span-2">
-            <PrimaryButton className="h-11 w-full">Submit</PrimaryButton>
+            <PrimaryButton className="h-11 w-full">{t("contact.form.submitButton.text")}</PrimaryButton>
           </div>
         </form>
       </div>
 
       {/* Contact Info Section */}
       <div className="self-center w-full space-y-5 md:w-1/2">
-        <h2 className="text-2xl font-bold">Contact Us</h2>
+        <h2 className="text-2xl font-bold">{t("contact.contactInfo.title")}</h2>
         <ContactInfo
           icon={<MapPin className="size-5 text-white" />}
-          label="Address"
-          content="3540 Toringdon Way, Suite 200 Charlotte, North Carolina 28277, United States"
+          label={t(`contact.contactInfo.items.0.label`)}
+          content={t(`contact.contactInfo.items.0.content`)}
         />
         <ContactInfo
           icon={<PhoneCall className="size-5 text-white" />}
-          label="Phone Number"
-          content="980-729-7442"
+          label={t(`contact.contactInfo.items.1.label`)}
+          content={t(`contact.contactInfo.items.1.content`)}
         />
         <ContactInfo
           icon={<MailsIcon className="size-5 text-white" />}
-          label="Email"
-          content="info@solarcellzusa.com"
+          label={t(`contact.contactInfo.items.2.label`)}
+          content={t(`contact.contactInfo.items.2.content`)}
         />
         <ContactInfo
           icon={<Home className="size-5 text-white" />}
-          label="Showroom"
-          content="10924 Granite St Suite 400 Charlotte NC 28273"
+          label={t(`contact.contactInfo.items.3.label`)}
+          content={t(`contact.contactInfo.items.3.content`)}
         />
       </div>
     </ResponsiveContainer>
