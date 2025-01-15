77efe6ba055fad3db6e6d96dac6561c468dc7b3b
dev78972343 <developer78972343@gmail.com>
2025-01-15 22:45:10 +0600

    feat: multilang added to demo section

--- src/components/Home/Demo/Demo.jsx
+++ src/components/Home/Demo/Demo.jsx
@@ -4,8 +4,10 @@ import {
   slideUpParentVariants,
 } from "@/utils/sharedMotionVariants";
 import { motion } from "motion/react";
+import { useTranslation } from "react-i18next";
 
 export default function Demo({ className }) {
+  const { t } = useTranslation();
   return (
     <motion.section
       variants={slideUpParentVariants()}
@@ -18,19 +20,19 @@ export default function Demo({ className }) {
       className={cn(className, "bg-gradient-to-br from-[#d4dee6] via-[#91C4E4] to-[#105FA8] py-20 lg:py-[150px] text-center")}
     >
       <motion.h1 variants={slideUpChildVariants} className="px-3 text-3xl lg:text-5xl font-bold">
-        Solar Power, Designed for the Future
+        {t("demo.title")}
       </motion.h1>
       <motion.p
         variants={slideUpChildVariants}
         className="mt-4 px-3 text-lg lg:text-xl text-gray-800"
       >
-        Discover innovative solar solutions that optimize energy efficiency, sustainability, and performance for homes and businesses.
+        {t("demo.description")}
       </motion.p>
       <motion.button
         variants={slideUpChildVariants}
         className="mt-8 rounded-3xl bg-primary-button-gradient px-10 py-3 text-white"
       >
-        Explore Our Demo
+        {t("demo.buttonText")}
       </motion.button>
     </motion.section>
   );
