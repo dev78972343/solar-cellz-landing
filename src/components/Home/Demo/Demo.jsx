78268145004e137996b2a19fcd6f4e9ec05d6b18
dev78972343 <developer78972343@gmail.com>
2025-01-29 01:27:49 +0600

    update demo section

--- src/components/Home/Demo/Demo.jsx
+++ src/components/Home/Demo/Demo.jsx
@@ -9,6 +9,7 @@ import { useTranslation } from "react-i18next";
 
 export default function Demo({ className }) {
   const { t } = useTranslation();
+  const demoContent = t("demo", { returnObjects: true });
   return (
     <motion.section
       variants={slideUpParentVariants()}
@@ -21,17 +22,17 @@ export default function Demo({ className }) {
       className={cn(className, "bg-gradient-to-br from-[#d4dee6] via-[#91C4E4] to-[#105FA8] py-20 lg:py-[150px] text-center")}
     >
       <motion.h1 variants={slideUpChildVariants} className="px-3 text-3xl lg:text-5xl font-bold">
-        {t("demo.title")}
+        {demoContent.title}
       </motion.h1>
       <motion.p
         variants={slideUpChildVariants}
         className="mt-4 px-3 text-lg lg:text-xl text-gray-800"
       >
-        {t("demo.description")}
+        {demoContent.description}
       </motion.p>
       <motion.div variants={slideUpChildVariants} className="mt-8">
         <PrimaryButton className="hover:shadow-lg rounded-3xl bg-primary-button-gradient px-10 py-3 text-white">
-          {t("demo.buttonText")}
+          {demoContent.buttonText}
         </PrimaryButton>
       </motion.div>
     </motion.section>
